c62461e3024203f77a84ab9030b897b6cf19de1b
андрей тулинов <tulinof30@yandex.ru>
2024-03-20 17:53:45 +0500

    first commit

---
+++ scripts/homeTask10.js
@@ -0,0 +1,12 @@
+const blockBl = document.querySelector('.blue');
+const blockRd = document.querySelector('.red');
+const cont = document.querySelector('#container')
+var slider = document.getElementById('myRange');
+
+slider.oninput = function() {
+    blockRd.style.left = (this.value/1.35) + '%';
+    blockBl.style.left = -(this.value/1.35) + '%';
+    cont.style.background = `rgb(${255-this.value*2.5}, ${255-this.value*2.5}, ${255-this.value*2.5})`;
+}
+
+
